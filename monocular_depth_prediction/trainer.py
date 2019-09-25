76a93995a3562e1a4ec76d6d4b515565e1daa922
Daniel Silva <danielzgsilva@knights.ucf.edu>
2019-09-25 14:08:25 -0400

    PyTorch Tensorboard

--- monocular_depth_prediction/trainer.py
+++ monocular_depth_prediction/trainer.py
@@ -13,7 +13,8 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from tensorboardX import SummaryWriter
+#from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 
 import json
 
